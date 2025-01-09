25a2a6f2a440ebe4f67684f3f7aa59f80e37c28e
BadSpidy <skmansoor002@gmail.com>
2025-01-09 15:38:14 +0530

    first commit second

---
+++ src/main/java/com/project/SpringSecurity/controller/SecurityController.java
@@ -0,0 +1,12 @@
+package com.project.SpringSecurity.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
