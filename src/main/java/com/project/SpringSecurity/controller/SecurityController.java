fc1532ae7760b89bfadac4aa39ce38027a61f6a9
BadSpidy <skmansoor002@gmail.com>
2025-01-09 16:24:20 +0530

    security push

--- src/main/java/com/project/SpringSecurity/controller/SecurityController.java
+++ src/main/java/com/project/SpringSecurity/controller/SecurityController.java
@@ -1,12 +1,17 @@
 package com.project.SpringSecurity.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
 
+    private static final Logger log = LogManager.getLogger(SecurityController.class);
+
     @Override
     public String toString() {
         return super.toString();
+
     }
 }
