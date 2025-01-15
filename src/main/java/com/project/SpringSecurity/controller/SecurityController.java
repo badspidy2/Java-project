20463982756d20da6771a9074f7e1f35c886c6ba
BadSpidy <skmansoor002@gmail.com>
2025-01-16 00:29:28 +0530

    Postmapping

--- src/main/java/com/project/SpringSecurity/controller/SecurityController.java
+++ src/main/java/com/project/SpringSecurity/controller/SecurityController.java
@@ -1,10 +1,19 @@
 package com.project.SpringSecurity.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class SecurityController {
 
-    
+    @PostMapping("/adddet")
+    public String addDetails(){
+
+        return "Added Successfully";
+
+    }
 
 }
